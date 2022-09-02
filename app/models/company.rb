debae1260b40b1388239e19f039610e6e7c835be
Steven-Otieno <steveotianga8991@gmail.com>
2022-09-02 22:12:59 +0300

    Assignment Done

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,13 @@
 class Company < ActiveRecord::Base
+  has_many :freebies
+  has_many :devs, through: :freebies
 
+  def give_freebies(dev, item, value)
+    Freebie.create(company_id: self.id,dev_id: dev, item_name: item,value: value)
+    self
+  end
+
+  def self.oldest_company
+    all.sort_by { |company| company.founding_year }.first
+  end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,20 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def recieved_one?(item)
+    freebies.all.find do |freebie|
+      freebie.item_name.include?(item)
+    end
+  end
+
+  def give_away(dev, freebie)
+    gift = freebies.find do |item|
+      item.item_name == freebie
+    end
+    if gift.dev_id == self.id
+      gift.dev_id = dev
+      gift.save
+    end
+  end
 end
